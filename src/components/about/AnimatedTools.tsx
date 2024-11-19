f0224a243d0de565b86abe9be96347c9dea9e0a3
Andersseen <andriipap01@gmail.com>
2024-11-19 19:43:35 +0100

    Update components

---
+++ src/components/about/AnimatedTools.tsx
@@ -0,0 +1,30 @@
+import React, { cloneElement } from "react";
+import Animated from "./Animated"; // Replace with the correct path to the "Animated" component
+
+type AnimatedToolsProps = {
+  delay: number;
+  stepSize: number;
+  children: React.ReactElement<React.ComponentProps<any>>[];
+  iconSize: number;
+  className?: string;
+};
+
+const AnimatedTools: React.FC<AnimatedToolsProps> = ({
+  delay,
+  stepSize,
+  children,
+  iconSize,
+  className,
+}) => {
+  return (
+    <div className={className}>
+      {children.map((child, index) => (
+        <Animated key={index} delay={delay + index * stepSize}>
+          {cloneElement(child, { size: iconSize })}
+        </Animated>
+      ))}
+    </div>
+  );
+};
+
+export default AnimatedTools;
